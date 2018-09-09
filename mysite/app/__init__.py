c66600c22ec2ac00216d57e488eb205b610ca0bd
frankiebaffa <frankiebaffa@gmail.com>
2018-09-09 14:52:11 -0400

    mvoe mail configuration to new file

--- mysite/app/__init__.py
+++ mysite/app/__init__.py
@@ -2,6 +2,7 @@
 from flask import Flask
 # get configuration from Config
 from config import Config
+from mail import MailConfig
 # SQLAlchemy for handling database
 from flask_sqlalchemy import SQLAlchemy
 # Migrate to handle db migrations
@@ -24,16 +25,8 @@ migrate = Migrate(app, db)
 # initialize login manager
 login = LoginManager(app)
 login.login_view = 'login'
-
-# configure the settings for flask_mail
-app.config["MAIL_SERVER"] = "smtp.gmail.com"
-app.config["MAIL_PORT"] = 465
-app.config["MAIL_USE_SSL"] = True
-app.config["MAIL_USERNAME"] = 'frankiebaffa.com@gmail.com'
-
-# Define the password within a local file 'local_settings.py' as:
-#       MAIL_PASSWORD = 'password-of-your-choice'
-app.config["MAIL_PASSWORD"] = local_settings.MAIL_PASSWORD
+# configure application from MailConfig
+MailConfig(app)
 
 mail = Mail()
 mail.init_app(app)
