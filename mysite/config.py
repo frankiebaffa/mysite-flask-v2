028668fac51d31f66d9a47f00eab3ad6383d4c32
frankiebaffa <frankiebaffa@gmail.com>
2018-09-09 17:36:34 -0400

    fix wording in comment

--- mysite/config.py
+++ mysite/config.py
@@ -7,8 +7,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 # create object Config for application configuration
 class Config(object):
-    # create variable SECRET_KEY as environment SECRET_KEY
+    # create variable SECRET_KEY equal to local_settings.SECRET_KEY
     SECRET_KEY = local_settings.SECRET_KEY
+
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
