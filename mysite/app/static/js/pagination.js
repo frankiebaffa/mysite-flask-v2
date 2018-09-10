01ed5978b10ed427cd1e5b4adc6cf10b10fe5ebb
frankiebaffa <frankiebaffa@gmail.com>
2018-09-09 21:59:50 -0400

    add code to alter color of active button

--- mysite/app/static/js/pagination.js
+++ mysite/app/static/js/pagination.js
@@ -9,10 +9,16 @@ $(document).ready(function pagination() {
   var links = [projectLinks, blogLinks, articleLinks];
   $(posts).each(function eachPost(index, element) {
     $(element[0]).show();
+    $(links[index][0]).removeClass('bg-darker fg-lighter');
+    $(links[index[0]]).addClass('bg-lightest fg-darker');
   });
   $(links).each(function eachLinkGroup(i, elements) {
     $(elements).each(function eachLink(j, element) {
       $(element).click(function postLinkOnClick() {
+        $(links[i]).removeClass('bg-lighter fg-darker');
+        $(links[i]).addClass('bg-darker fg-lighter');
+        $(element).removeClass('bg-darker fg-lighter');
+        $(element).addClass('bg-lightest fg-darker');
         $(posts[i]).hide();
         $(posts[i][j]).show();
       });
