bf014d112fbefa9ec5d1ad27da276b1200c5d4e4
frankiebaffa <frankiebaffa@gmail.com>
2018-09-18 22:01:41 -0400

    add routes for managing and viewing reviews

--- mysite/app/routes.py
+++ mysite/app/routes.py
@@ -8,7 +8,7 @@ from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, login_required
 from flask_login import logout_user
 # from models import User for login query
-from app.models import User, Blog, Project
+from app.models import User, Blog, Project, Review
 # get forms from app/forms.py
 from app.forms import LoginForm, ContactForm, ContentForm
 # get flask_mail for contact form
@@ -23,27 +23,60 @@ import feedparser
 def index():
     projects = Project.query.order_by(Project.id.desc()).limit(5).all()
     blog = Blog.query.order_by(Blog.id.desc()).limit(5).all()
+    reviews = Review.query.order_by(Review.id.desc()).limit(5).all()
     pagetype = "top"
-    # return template at 'pages/index.html'
-    return render_template('pages/index.html', projects=projects, blog=blog, pagetype=pagetype)
+    return render_template('pages/index.html', projects=projects,
+            blog=blog, reviews=reviews, pagetype=pagetype)
 
 @app.route('/blog/<post>', methods=['GET'])
 def blogpost(post):
     blog = Blog.query.filter(Blog.id == post).first()
+    title = blog.title
     pagetype = "single"
     back = url_for('blog') + '#blog-container-' + str(post)
-    return render_template("pages/blogpost.html", blog=blog, pagetype=pagetype, back=back)
+    return render_template("pages/blog.html", title=title, blog=blog, pagetype=pagetype,
+            back=back)
 
 @app.route('/blog')
 def blog():
-    blog = Blog.query.all()
+    title = "Blogs"
+    blog = Blog.query.order_by(Blog.id.desc()).all()
     pagetype = "all"
-    return render_template("pages/blog.html", blog=blog, pagetype=pagetype)
+    return render_template("pages/blog.html", title=title, blog=blog, pagetype=pagetype)
 
-@app.route('/project/<post>', methods=['GET'])
+@app.route('/projects/<post>', methods=['GET'])
 def projectpost(post):
-    projectpost = Project.query.filter(Project.id == post).first()
-    return render_template("pages/projectpost.html", projectpost=projectpost)
+    pagetype = "single"
+    project = Project.query.filter(Project.id == post).first()
+    title = project.title
+    back = url_for('projects') + '#project-container-' + str(post)
+    return render_template("pages/projects.html", title=title, project=project,
+            pagetype=pagetype, back=back)
+
+@app.route('/projects')
+def projects():
+    title = "Projects"
+    projects = Project.query.order_by(Project.id.desc()).all()
+    pagetype = "all"
+    return render_template("pages/projects.html", title=title, projects=projects,
+            pagetype=pagetype)
+
+@app.route('/reviews')
+def reviews():
+    title = "Reviews"
+    reviews = Review.query.order_by(Review.id.desc()).all()
+    pagetype = "all"
+    return render_template("pages/reviews.html", title=title, reviews=reviews,
+            pagetype=pagetype)
+
+@app.route('/reviews/<post>', methods=['GET'])
+def reviewpost(post):
+    pagetype = "single"
+    review = Review.query.filter(Review.id == post).first()
+    title = review.title
+    back = url_for('reviews') + '#review-container-' + str(post)
+    return render_template("pages/reviews.html", title=title, review=review,
+            pagetype=pagetype, back=back)
 
 
 ## create route 'news' at '/news'
@@ -60,6 +93,7 @@ def projectpost(post):
 # if form validate on submit, log user in
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    title = "Login"
     form = LoginForm()
     if current_user.is_authenticated:
         return redirect(url_for('manage'))
@@ -71,28 +105,30 @@ def login():
             return redirect(url_for('login'))
         login_user(user)
         return redirect(url_for('manage'))
-    return render_template('pages/login.html', title='Sign In', form=form)
+    return render_template('pages/login.html', title=title, form=form)
 
 # create route to contact method
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
+    title = "Contact"
     mail = Mail()
     form = ContactForm()
     if request.method == 'POST':
         if form.validate() == False:
-            return render_template('pages/contact.html', form=form)
+            return render_template('pages/contact.html', title=title, form=form)
         else:
             msg = Message(form.subject.data, sender=app.config['MAIL_USERNAME'],
-                recipients=[app.config['CONTACT_EMAIL']])
+                    recipients=[app.config['CONTACT_EMAIL']])
             msg.body = ContactEmail(form.name.data, form.email.data, form.body.data)
             mail.send(msg)
-            confirm = Message('Contact Confirmation', sender=app.config['MAIL_USERNAME'],
-                recipients=[form.email.data])
+            confirm = Message('Contact Confirmation',
+                    sender=app.config['MAIL_USERNAME'],
+                    recipients=[form.email.data])
             confirm.body = ConfirmationEmail(form.name.data)
             mail.send(confirm)
             return redirect(url_for('contact'))
     elif request.method == 'GET':
-        return render_template('pages/contact.html', form=form)
+        return render_template('pages/contact.html', title=title, form=form)
 
 #=======================================#
 #       All below methods must          #
@@ -104,12 +140,14 @@ def contact():
 @app.route('/manage')
 @login_required
 def manage():
+    title = "Manage Content"
     projects = Project.query.all()
     blogs = Blog.query.all()
-    content = [projects, blogs]
+    reviews = Review.query.all()
+    content = [projects, blogs, reviews]
     form = ContentForm()
-    return render_template('pages/manage.html', content=content,
-            form=form, projects=projects, blogs=blogs)
+    return render_template('pages/manage.html', title=title, content=content,
+            form=form, projects=projects, blogs=blogs, reviews=reviews)
 
 @app.route('/manage/createproject', methods=['POST'])
 @login_required
@@ -163,6 +201,37 @@ def editblog():
     db.session.add(blog)
     db.session.commit()
     return redirect(url_for('manage'))
+
+@app.route('/manage/createreview', methods=['POST'])
+@login_required
+def createreview():
+    title = request.form.get("title")
+    body = request.form.get("body")
+    url = request.form.get("url")
+    repo = request.form.get("repo")
+    review = Review(title=title, body=body, url=url, repo=repo,
+            user_id=current_user.id)
+    db.session.add(review)
+    db.session.commit()
+    return redirect(url_for('manage'))
+
+@app.route('/manage/editreview', methods=['POST'])
+@login_required
+def editreview():
+    review_id = request.form.get("id")
+    title = request.form.get("title")
+    body = request.form.get("body")
+    url = request.form.get("url")
+    repo = request.form.get("repo")
+    review = Review.query.filter(Review.id==review_id).first()
+    review.title = title
+    review.body = body
+    review.url = url
+    review.repo = repo
+    db.session.add(review)
+    db.session.commit()
+    return redirect(url_for('manage'))
+
 
 # create route to logout method
 # only allow if user is logged in
